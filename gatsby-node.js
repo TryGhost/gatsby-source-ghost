86cacf9954d2b1253f61bcef23cc8ace21726495
Aileen Nowak <aileen@ghost.org>
2019-01-08 19:35:41 +0700

    Postcount for tags and authors (#8)
    - Added post count information to tags and authors, so that it's possible to generate the right number of page urls in Gatsby during the build process.
    
    * ➕ Added lodash
    * ✨ Add post count per tag
    * ✨ Author post count
    * 🚚 Rename fn to `getPostCount`

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,5 +1,27 @@
 const GhostAPI = require('./api');
+const _ = require('lodash');
 const {PostNode, PageNode, TagNode, AuthorNode} = require('./nodes');
+
+const getPostCount = function getPostCount(posts, taxonomie) {
+    let allTaxonomies = [];
+
+    // Get all possible taxonimies that are being used and
+    // create a usable array
+    posts.forEach(post => allTaxonomies.push(post[taxonomie]));
+    allTaxonomies = _.flatten(allTaxonomies);
+    allTaxonomies = _.transform(_.uniqBy(allTaxonomies, item => item.id), (result, item) => {
+        (result[item.slug] || (result[item.slug] = 0));
+    }, {});
+
+    // Now collect all post slugs per taxonomie
+    posts.forEach((post) => {
+        post[taxonomie].forEach((item) => {
+            allTaxonomies[item.slug] += 1;
+        });
+    });
+
+    return allTaxonomies;
+};
 
 exports.sourceNodes = ({boundActionCreators}, configOptions) => {
     const {createNode} = boundActionCreators;
@@ -7,6 +29,9 @@ exports.sourceNodes = ({boundActionCreators}, configOptions) => {
     return GhostAPI
         .fetchAllPosts(configOptions)
         .then((posts) => {
+            const tagPostCount = getPostCount(posts, 'tags');
+            const authorPostCount = getPostCount(posts, 'authors');
+
             posts.forEach((post) => {
                 if (post.page) {
                     createNode(PageNode(post));
@@ -16,12 +41,18 @@ exports.sourceNodes = ({boundActionCreators}, configOptions) => {
 
                 if (post.tags) {
                     post.tags.forEach((tag) => {
+                        // find the number of posts that have this tag
+                        tag.postCount = tagPostCount[tag.slug] || 0;
+
                         createNode(TagNode(tag));
                     });
                 }
 
                 if (post.authors) {
                     post.authors.forEach((author) => {
+                        // find the number of posts that include this author
+                        author.postCount = authorPostCount[author.slug] || 0;
+
                         createNode(AuthorNode(author));
                     });
                 }
