c5f0c54749974c36e9d6846ad609369893e532d7
Aileen Nowak <AileenCGN@gmail.com>
2019-01-08 12:33:16 +0700

    ✨ Add post count per tag

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,5 +1,26 @@
 const GhostAPI = require('./api');
+const _ = require('lodash');
 const {PostNode, PageNode, TagNode, AuthorNode} = require('./nodes');
+
+const getPostsCountPerTag = function getPostsCountPerTag(posts) {
+    let allTags = [];
+
+    // Get all possible tags that are being used
+    posts.forEach(post => allTags.push(post.tags));
+    allTags = _.flatten(allTags);
+    allTags = _.transform(_.uniqBy(allTags, tag => tag.id), (result, tag) => {
+        (result[tag.slug] || (result[tag.slug] = []));
+    }, []);
+
+    // collect all post slugs per tag
+    posts.forEach((post) => {
+        post.tags.forEach((tag) => {
+            allTags[tag.slug].push(post.slug);
+        });
+    });
+
+    return allTags;
+};
 
 exports.sourceNodes = ({boundActionCreators}, configOptions) => {
     const {createNode} = boundActionCreators;
@@ -7,6 +28,8 @@ exports.sourceNodes = ({boundActionCreators}, configOptions) => {
     return GhostAPI
         .fetchAllPosts(configOptions)
         .then((posts) => {
+            const tagPostCount = getPostsCountPerTag(posts);
+
             posts.forEach((post) => {
                 if (post.page) {
                     createNode(PageNode(post));
@@ -16,6 +39,9 @@ exports.sourceNodes = ({boundActionCreators}, configOptions) => {
 
                 if (post.tags) {
                     post.tags.forEach((tag) => {
+                        // find the number of posts that have this tag
+                        tag.postCount = tagPostCount[tag.slug].length;
+
                         createNode(TagNode(tag));
                     });
                 }
