24ed713ce2f37e0ba3e8c1ff8c23985fe704eb94
Hannah Wolfe <erisds@gmail.com>
2019-01-15 16:14:14 +0000

    Misc cleanup
    - add a missing comment
    - move the temporary code below the real code :)

--- gatsby-node.js
+++ gatsby-node.js
@@ -3,32 +3,6 @@ const ContentAPI = require('./content-api');
 const {PostNode, PageNode, TagNode, AuthorNode, SettingsNode, fakeNodes} = require('./ghost-nodes');
 
 /**
- * Create Temporary Fake Nodes
- * Refs: https://github.com/gatsbyjs/gatsby/issues/10856#issuecomment-451701011
- * Ensures that Gatsby knows about every field in the Ghost schema
- */
-const createTemporaryFakeNodes = ({emitter, actions}) => {
-    // Setup our temporary fake nodes
-    fakeNodes.forEach((node) => {
-        // createTemporaryFakeNodes is called twice. The second time, the node already has an owner
-        // This triggers an error, so we clean the node before trying again
-        delete node.internal.owner;
-        actions.createNode(node);
-    });
-
-    const onSchemaUpdate = () => {
-        // Destroy our temporary fake nodes
-        fakeNodes.forEach((node) => {
-            actions.deleteNode({node});
-        });
-        emitter.off(`SET_SCHEMA`, onSchemaUpdate);
-    };
-
-    // Use a Gatsby internal API to cleanup our Fake Nodes
-    emitter.on(`SET_SCHEMA`, onSchemaUpdate);
-};
-
-/**
  * Create Live Ghost Nodes
  * Uses the Ghost Content API to fetch all posts, pages, tags, authors and settings
  * Creates nodes for each record, so that they are all available to Gatsby
@@ -72,11 +46,38 @@ const createLiveGhostNodes = ({actions}, configOptions) => {
     });
 
     const fetchSettings = api.settings.browse().then((setting) => {
+        // The settings object doesn't have an id, prevent Gatsby from getting 'undefined'
         setting.id = 1;
         createNode(SettingsNode(setting));
     });
 
     return Promise.all([fetchPosts, fetchPages, fetchTags, fetchAuthors, fetchSettings]);
+};
+
+/**
+ * Create Temporary Fake Nodes
+ * Refs: https://github.com/gatsbyjs/gatsby/issues/10856#issuecomment-451701011
+ * Ensures that Gatsby knows about every field in the Ghost schema
+ */
+const createTemporaryFakeNodes = ({emitter, actions}) => {
+    // Setup our temporary fake nodes
+    fakeNodes.forEach((node) => {
+        // createTemporaryFakeNodes is called twice. The second time, the node already has an owner
+        // This triggers an error, so we clean the node before trying again
+        delete node.internal.owner;
+        actions.createNode(node);
+    });
+
+    const onSchemaUpdate = () => {
+        // Destroy our temporary fake nodes
+        fakeNodes.forEach((node) => {
+            actions.deleteNode({node});
+        });
+        emitter.off(`SET_SCHEMA`, onSchemaUpdate);
+    };
+
+    // Use a Gatsby internal API to cleanup our Fake Nodes
+    emitter.on(`SET_SCHEMA`, onSchemaUpdate);
 };
 
 // Standard way to create nodes
