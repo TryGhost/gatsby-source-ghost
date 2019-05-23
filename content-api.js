15028a89a85a3ae3b024e92143a21f6ca64fbe1b
Aileen Nowak <AileenCGN@gmail.com>
2019-05-23 10:25:49 +0800

    🎨 Updated content API parameter from host to url
    no issue
    
    - Content API parameter `host` is deprecated and needs to be switched to `url` instead

--- content-api.js
+++ content-api.js
@@ -1,7 +1,7 @@
 const GhostContentAPI = require('@tryghost/content-api');
 
 module.exports.configure = configOptions => new GhostContentAPI({
-    host: configOptions.apiUrl,
+    url: configOptions.apiUrl,
     key: configOptions.contentApiKey,
     version: 'v2'
 });
